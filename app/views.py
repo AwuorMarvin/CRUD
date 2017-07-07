667ed9137d3a0a3055a40c4769c37901152a6eaa
Awuor Marvin <awuor.marvin5@gmail.com>
2017-07-07 23:08:28 +0300

    Create base layout

--- app/views.py
+++ app/views.py
@@ -3,5 +3,12 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # return render_template('index.html')
+    return render_template('base.html')
     # return 'hello from the other side'
+
+def login():
+    # return render_template('index.html')
+    return render_template('login.html')
+    # return 'hello from the other side'
+    
