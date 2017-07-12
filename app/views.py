eca817ed4ab55e8fbd38532c8d60b801bc153056
Awuor Marvin <awuor.marvin5@gmail.com>
2017-07-12 10:29:57 +0300

    Create login route

--- app/views.py
+++ app/views.py
@@ -4,9 +4,10 @@ from app import app
 @app.route('/')
 def index():
     # return render_template('index.html')
-    return render_template('base.html')
+    return render_template('index.html')
     # return 'hello from the other side'
 
+@app.route('/login')
 def login():
     # return render_template('index.html')
     return render_template('login.html')
